f7d94ede625312490269f2c19ab6d612d1439c7f
Luke <02luke.marchand@gulllakecs.org>
2021-04-16 13:00:25 -0400

    extra changes

---
+++ app/src/main/java/com/example/android/miwoklanguageapp/NumbersActivity.java
@@ -0,0 +1,16 @@
+package com.example.android.miwoklanguageapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class NumbersActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_numbers);
+    }
+}
