fd5ce16d60102a59099a652b749eeefb13e2ed11
Luke <02luke.marchand@gulllakecs.org>
2021-04-26 12:46:39 -0400

    Changed words array to an ArrayList

--- app/src/main/java/com/example/android/miwoklanguageapp/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwoklanguageapp/NumbersActivity.java
@@ -7,28 +7,30 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 
+import java.util.ArrayList;
+
 public class NumbersActivity extends AppCompatActivity {
 
-    String[] words = new String[10];
+    ArrayList<String> words = new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
 
-        words[0] = "one";
-        words[1] = "two";
-        words[2] = "three";
-        words[3] = "four";
-        words[4] = "five";
-        words[5] = "six";
-        words[6] = "seven";
-        words[7] = "eight";
-        words[8] = "nine";
-        words[9] = "ten";
+        words.add("one");
+        words.add("two");
+        words.add("three");
+        words.add("four");
+        words.add("five");
+        words.add("six");
+        words.add("seven");
+        words.add("eight");
+        words.add("nine");
+        words.add("ten");
 
-        for (int i = 0; i < words.length; i++){
-            Log.v("NumbersActivity", "Word at index " + i + ": " + words[i]);
+        for (int i = 0; i < words.size(); i++){
+            Log.v("NumbersActivity", "Word at index " + i + ": " + words.get(i));
         }
     }
 }
