683a8abcc9503ff36739abd1fe8cdfaded988e61
Luke <02luke.marchand@gulllakecs.org>
2021-04-26 12:05:52 -0400

    Added array of number words

--- app/src/main/java/com/example/android/miwoklanguageapp/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwoklanguageapp/NumbersActivity.java
@@ -4,13 +4,27 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 public class NumbersActivity extends AppCompatActivity {
+
+    String[] words = new String[10];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
+
+        words[0] = "one";
+        words[1] = "two";
+        words[2] = "three";
+        words[3] = "four";
+        words[4] = "five";
+        words[5] = "six";
+        words[6] = "seven";
+        words[7] = "eight";
+        words[8] = "nine";
+        words[9] = "ten";
     }
 }
